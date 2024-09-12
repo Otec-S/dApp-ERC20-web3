440b4920ccd2327b310057c8bb2a1dd6f29d4737
Vadim Manushin <vadimmanushin@yandex.ru>
2024-09-12 14:07:13 +0300

    added my own burger with styles correction

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,32 +1,35 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import styles from './Header.module.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import './Burger.css';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { slide as BurgerMenu } from 'react-burger-menu';
 
 import imgUrl from '../../assets/logo.svg';
 
 const Header = (): React.ReactElement => {
-  const positionRight = false;
+
+  const [burgerIsActive, setBurger] = useState(false);
+  const handleBurgerClick = (): void => {
+    setBurger(!burgerIsActive);
+  };
+
+  let burgerClasses = styles.burger;
+  burgerClasses += burgerIsActive ? ` ${styles.burgerActive}` : '';
+
   return (
     <header className={styles.header}>
       <div className={styles.burgerWrapper}>
-        <BurgerMenu right={positionRight}>
-          <NavLink className={styles.navLink} to="/">
-            Send ERC-20
-          </NavLink>
-          <NavLink className={styles.navLink} to="/">
-            Trade ERC-20
-          </NavLink>
-          <NavLink className={styles.navLink} to="/">
-            NFT Collection
-          </NavLink>
-        </BurgerMenu>
+        <button
+            type='button'
+            className={burgerClasses}
+            onClick={handleBurgerClick}
+          >
+            <div className={styles.burgerImage} />
+          </button>
       </div>
-      <img src={imgUrl} alt="Project logo" />
-      <div className={styles.body}>
-        <nav className={styles.nav}>
+      <img className={burgerIsActive ? styles.imgBurgerIsOpen : ''} src={imgUrl} alt="Project logo" />
+      <div className={burgerIsActive ? styles.bodyBurgerIsOpen :styles.body}>
+        <nav className={burgerIsActive? styles.navBurgerIsOpen : styles.nav}>
           <NavLink className={styles.navLink} to="/">
             Send ERC-20
           </NavLink>
@@ -37,8 +40,15 @@ const Header = (): React.ReactElement => {
             NFT Collection
           </NavLink>
         </nav>
-        <ConnectButton label="Connect wallet" showBalance={false}/>
+        <div className={styles.connectButtonWrapper}>
+          <ConnectButton 
+            label="Connect wallet" 
+            chainStatus={"none"}
+            showBalance={false}
+            accountStatus="address"
+          />
       </div>
+        </div>
     </header>
   );
 };
