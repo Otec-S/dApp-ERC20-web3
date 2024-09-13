e52a702c14b2811e3cc23c537bec53ef42c569c6
Sergey Grigorash <adviser@bk.ru>
2024-09-13 10:02:40 +0500

    feat make second form in SendERC20Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import SendERC20Component from "./components/send-ERC-20-component/send-ERC-20-component";
+import SendERC20Component from "./components/send-ERC-20-component/Send-ERC-20-component";
 
 function App() {
   return <SendERC20Component />;
--- src/components/send-ERC-20-component/send-ERC-20-component.tsx
+++ src/components/send-ERC-20-component/send-ERC-20-component.tsx
@@ -1,21 +1,53 @@
-import style from "./send-ERC-20-component.module.css";
+import style from "./Send-ERC-20-component.module.css";
 import balanceMaxSign from "../../assets/balanceMaxSign.svg";
 import USDTLogo from "../../assets/USDTLogo.svg";
 import arrow_down from "../../assets/arrow_down.svg";
+import { useState } from "react";
 
 const SendERC20Component = () => {
+  const [inputValue, setInputValue] = useState(0);
+  const [recipientValue, setRecipientValue] = useState("");
+  // TODO: временно хардкодим баланс, потом будет приходить с сервера
+  const balance = 5800;
+  const formattedBalance = balance.toLocaleString("en-US");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Form submitted");
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(event.target.value);
+    setInputValue(isNaN(value) ? 0 : value);
+  };
+
+  const handleRecipientChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRecipientValue(event.target.value);
+  };
+
+  const isButtonActive = inputValue > 0 && recipientValue.length > 0;
+
   return (
     <>
       <main className={style.component}>
         <section className={style.block}>
           <div className={style.blockTitle}>Send ERC-20</div>
-          <div className={style.blockForm}>
+          <form className={style.blockForm} onSubmit={handleSubmit}>
             <div className={style.sender}>
               <div className={style.inputBlock}>
                 {/* TODO: должны быть запятые как разделители разрядов? */}
-                <input className={style.input} type="text" value="0" />
+                <input
+                  className={style.input}
+                  type="text"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                />
                 <div className={style.balance}>
-                  <div className={style.balanceValue}>Balance 5,800.00</div>
+                  <div className={style.balanceValue}>
+                    Balance {formattedBalance}
+                  </div>
                   <img src={balanceMaxSign} alt="Max balance icon" />
                 </div>
               </div>
@@ -41,10 +73,19 @@ const SendERC20Component = () => {
               <input
                 className={style.recipientInput}
                 placeholder="0x0000000000000000000000000000000000000000"
+                value={recipientValue}
+                onChange={handleRecipientChange}
               />
             </div>
-            <button className={style.button}>Send</button>
-          </div>
+            <button
+              className={`${style.button} ${
+                isButtonActive ? style.activeButton : ""
+              }`}
+              disabled={!isButtonActive}
+            >
+              Send
+            </button>
+          </form>
         </section>
       </main>
     </>
