765a076f5c8b8a20cfe2319d15d718ccf5bade6c
Galia Muslimova <galia.musllimova@gmail.com>
2024-09-13 15:11:56 +0500

    change structure

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { collection, landing, sendErc, tradeErc } from '../../assets/images';
 import classes from './Landing.module.css';
-import { landing, sendErc, tradeErc, collection } from '../../assets/images';
 
 export const Landing: React.FC = () => {
+  const navigate = useNavigate();
+
+  const blockData = [
+    {
+      title: 'Send ERC-20',
+      description: 'Send ERC-20 tokens quickly and without fees!',
+      text: 'Our product allows you to transfer any amount of tokens directly, without intermediaries or extra costs. Simply select the recipient, specify the amount, and your transaction is instantly completed. Convenient and secure asset management without limitations!',
+      link: '/',
+      classname: classes.sendBlock,
+      imageSrc: sendErc.default,
+    },
+    {
+      title: 'Trade ERC-20',
+      description: 'Token exchange made simple and secure!',
+      text: 'Create and accept token exchange offers with other users. Specify which tokens you want to give and receive, and choose the recipient. Tokens are stored in a smart contract until the transaction is complete. Trade tokens with minimal fees and full transparency!',
+      link: '/',
+      classname: classes.tradeBlock,
+      imageSrc: tradeErc.default,
+    },
+    {
+      title: 'NFT Collection',
+      description: 'Unlock unique opportunities with our NFT collection!',
+      text: 'Each token is a unique piece of digital art and your access to exclusive company products and services. Join our community of collectors and gain privileges available only to our NFT holders. Invest in the future and enjoy exclusive benefits!',
+      link: '/',
+      classname: classes.collectionBlock,
+      imageSrc: collection.default,
+    },
+  ];
+
+  const statisticData = [
+    { text: 'Over 200,000', description: 'Clients from different countries' },
+    { text: 'Over 22,000', description: 'Orders per week' },
+    { text: 'Over 200', description: 'DeFi projects' },
+  ];
+
   return (
     <div className={classes.container}>
       <div className={classes.imageSection}>
@@ -14,67 +50,33 @@ export const Landing: React.FC = () => {
       </div>
       <div className={classes.section}>
         <h2>Products</h2>
-        <div className={classes.sendBlock}>
-          <div className={classes.blockTextGroup}>
-            <h3>Send ERC-20</h3>
-            <p>Send ERC-20 tokens quickly and without fees!</p>
-            <p>
-              Our product allows you to transfer any amount of tokens directly, without intermediaries or extra costs.
-              Simply select the recipient, specify the amount, and your transaction is instantly completed. Convenient
-              and secure asset management without limitations!
-            </p>
-            <button className={classes.blockButton}>Go to app</button>
+        {blockData.map((item) => (
+          <div className={item.classname}>
+            <div className={classes.blockTextGroup}>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <p>{item.text}</p>
+              <button className={classes.blockButton} onClick={() => navigate(item.link)}>
+                Go to app
+              </button>
+            </div>
+            <div className={classes.blockImgWrap}>
+              <Link to={item.link}>
+                <img src={item.imageSrc} className={classes.blockImg} />
+              </Link>
+            </div>
           </div>
-          <div className={classes.blockImgWrap}>
-            <img src={sendErc.default} className={classes.blockImg} />
-          </div>
-        </div>
-        <div className={classes.tradeBlock}>
-          <div className={classes.blockImgWrap}>
-            <img src={tradeErc.default} className={classes.blockImg} />
-          </div>
-          <div className={classes.blockTextGroup}>
-            <h3>Trade ERC-20</h3>
-            <p>Token exchange made simple and secure!</p>
-            <p>
-              Create and accept token exchange offers with other users. Specify which tokens you want to give and
-              receive, and choose the recipient. Tokens are stored in a smart contract until the transaction is
-              complete. Trade tokens with minimal fees and full transparency!
-            </p>
-            <button className={classes.blockButton}>Go to app</button>
-          </div>
-        </div>
-        <div className={classes.collectionBlock}>
-          <div className={classes.blockTextGroup}>
-            <h3>NFT Collection</h3>
-            <p>Unlock unique opportunities with our NFT collection!</p>
-            <p>
-              Each token is a unique piece of digital art and your access to exclusive company products and services.
-              Join our community of collectors and gain privileges available only to our NFT holders. Invest in the
-              future and enjoy exclusive benefits!
-            </p>
-            <button className={classes.blockButton}>Go to app</button>
-          </div>
-          <div className={classes.blockImgWrap}>
-            <img src={collection.default} className={classes.blockImg} />
-          </div>
-        </div>
+        ))}
       </div>
       <div className={classes.section}>
         <h2>Statistics</h2>
         <div className={classes.statisticBlock}>
-          <div className={classes.statisticItem}>
-            <p className={classes.statisticText}>Over 200,000</p>
-            <p className={classes.statisticDescription}>Clients from different countries</p>
-          </div>
-          <div className={classes.statisticItem}>
-            <p className={classes.statisticText}>Over 22,000</p>
-            <p className={classes.statisticDescription}>Orders per week</p>
-          </div>
-          <div className={classes.statisticItem}>
-            <p className={classes.statisticText}>Over 200</p>
-            <p className={classes.statisticDescription}>DeFi projects</p>
-          </div>
+          {statisticData.map((item) => (
+            <div className={classes.statisticItem}>
+              <p className={classes.statisticText}>{item.text}</p>
+              <p className={classes.statisticDescription}>{item.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
