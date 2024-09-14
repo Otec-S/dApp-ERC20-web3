75b2df9e074f14b9ec08691c88cf797aa7beef2f
Sergey Grigorash <adviser@bk.ru>
2024-09-14 12:49:50 +0500

    feat add header to Send-ERC20 component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { FC } from 'react';
 
 import Landing from './pages/Landing/Landing';
 import './App.css';
+import SendERC20Component from './components/send-ERC-20-component/send-ERC-20-component';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Landing />,
+  },
+  {
+    path: '/send-ERC20-tokens',
+    element: <SendERC20Component />,
   },
 ]);
 
---
+++ src/pages/SendERC20/SendERC20.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SendERC20 = () => {
+  return <div></div>;
+};
+
+export default SendERC20;
