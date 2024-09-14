432037cd8db5ba12d56e225cc3149011dda6d77e
Galia Muslimova <galia.musllimova@gmail.com>
2024-09-14 13:25:17 +0500

    move styles, fix prettier impport, add alt for images

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -42,34 +42,34 @@ export const Landing: React.FC = () => {
   return (
     <div className={classes.container}>
       <div className={classes.imageSection}>
-        <img src={landing.default} className={classes.mainImage} />
+        <img src={landing.default} className={classes.mainImage} alt={'main image'} />
         <div className={classes.title}>
-          <h1>Web3 platform for everything in crypto</h1>
+          <h1 className={classes.titleText}>Web3 platform for everything in crypto</h1>
         </div>
         <button className={classes.button}>Start using now</button>
       </div>
       <div className={classes.section}>
-        <h2>Products</h2>
+        <h2 className={classes.sectionTitle}>Products</h2>
         {blockData.map((item) => (
           <div className={item.classname}>
             <div className={classes.blockTextGroup}>
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-              <p>{item.text}</p>
+              <h3 className={classes.blockTitle}>{item.title}</h3>
+              <p className={classes.text}>{item.description}</p>
+              <p className={classes.text}>{item.text}</p>
               <button className={classes.blockButton} onClick={() => navigate(item.link)}>
                 Go to app
               </button>
             </div>
             <div className={classes.blockImgWrap}>
               <Link to={item.link}>
-                <img src={item.imageSrc} className={classes.blockImg} />
+                <img src={item.imageSrc} className={classes.blockImg} alt={item.title} />
               </Link>
             </div>
           </div>
         ))}
       </div>
       <div className={classes.section}>
-        <h2>Statistics</h2>
+        <h2 className={classes.sectionTitle}>Statistics</h2>
         <div className={classes.statisticBlock}>
           {statisticData.map((item) => (
             <div className={classes.statisticItem}>
