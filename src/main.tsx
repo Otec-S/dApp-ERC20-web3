3746368e56cff955121922e070f4efecec5dcb1f
Sergey Grigorash <adviser@bk.ru>
2024-09-16 23:27:03 +0500

    chore delete unnecessary components

--- src/main.tsx
+++ src/main.tsx
@@ -3,27 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* тут конфиг из wagmi  */}
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          // TODO: добавил initialChain
-          initialChain={3}
-          modalSize="compact"
-          theme={darkTheme({
-            accentColor: '#2D4BC1',
-            accentColorForeground: '#FFF',
-            borderRadius: 'small',
-            overlayBlur: 'small',
-          })}
-        >
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <App />
   </StrictMode>,
 );
