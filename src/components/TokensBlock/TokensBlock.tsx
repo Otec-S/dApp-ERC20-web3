5da14e1991bb1e68e905472451b29c612608f09e
Galia Muslimova <galia.musllimova@gmail.com>
2024-09-16 18:52:18 +0500

    check connection

--- src/components/TokensBlock/TokensBlock.tsx
+++ src/components/TokensBlock/TokensBlock.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { tokens } from '@src/assets/constants';
+import { useConnections } from 'wagmi';
 import { TokenRow } from '../TokenRow';
 import classes from './TokensBlock.module.css';
 
 export const TokensBlock: React.FC = () => {
+  const connections = useConnections();
+
+  console.log(connections);
+
   return (
     <div className={classes.tokenContainer}>
-      {tokens.map((token) => (
-        <TokenRow
-          icon={token.icon}
-          sepoliaAddress={token.sepoliaAddress}
-          polygonAddress={token.polygonAddress}
-          key={token.id}
-        />
-      ))}
+      {connections.length > 0 ? (
+        tokens.map((token) => (
+          <TokenRow
+            icon={token.icon}
+            sepoliaAddress={token.sepoliaAddress}
+            polygonAddress={token.polygonAddress}
+            key={token.id}
+          />
+        ))
+      ) : (
+        <h3>Connect to wallet</h3>
+      )}
     </div>
   );
 };
