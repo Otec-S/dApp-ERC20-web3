4cf7123d88059aa926c6fbc97d6e82ef576d0e85
Galia Muslimova <galia.musllimova@gmail.com>
2024-09-17 17:43:55 +0500

    add chain checking

--- src/components/TokensBlock/TokensBlock.tsx
+++ src/components/TokensBlock/TokensBlock.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useChainId, useConnections } from 'wagmi';
 import { TokenRow } from '../TokenRow';
+import { polygonId, sepoliaId } from './TokensBlock.constants';
 import { getTokens } from './TokensBlock.utils';
 import classes from './TokensBlock.module.css';
 
 export const TokensBlock: React.FC = () => {
   const connections = useConnections();
   const chainId = useChainId();
-  const tokens = getTokens(chainId);
+  const isExistChain = chainId === sepoliaId || chainId === polygonId;
+  const tokens = isExistChain ? getTokens(chainId) : null;
 
   return (
     <div className={classes.tokenContainer}>
       {connections.length > 0 ? (
-        tokens.map((token) => <TokenRow icon={token.icon} address={token.address} key={token.id} />)
+        tokens?.map((token) => <TokenRow icon={token.icon} address={token.address} key={token.id} />)
       ) : (
-        <h3>Connect to wallet</h3>
+        <h3>Connect to Sepolia or PolygonAmoy</h3>
       )}
     </div>
   );
