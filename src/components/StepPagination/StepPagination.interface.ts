502f12a1f91cdeefc01c276972869d2363b772ea
Vadim Manushin <vadimmanushin@yandex.ru>
2024-09-25 16:58:55 +0300

    added trade abi and step pagination

---
+++ src/components/StepPagination/StepPagination.interface.ts
@@ -0,0 +1,10 @@
+export enum StepStatus {
+  DISABLED = 'disabled',
+  DARK = 'dark',
+  LIGHT = 'light',
+  COMPLETED = 'completed',
+}
+export interface Step {
+  value: number;
+  status: StepStatus;
+}
---
+++ src/components/StepPagination/StepPagination.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import cn from 'classnames';
+
+import CheckIcon from '@assets/icons/check_icon.svg';
+
+import { Step, StepStatus } from './StepPagination.interface';
+import styles from './StepPagination.module.css';
+
+type Props = {
+  steps: Step[];
+};
+
+export const StepPagination: FC<Props> = ({ steps }) => {
+  return (
+    <div className={styles.stepRow}>
+      {steps.map((step) => (
+        <div
+          className={cn(styles.step, {
+            [styles.stepLight]: step.status == StepStatus.LIGHT,
+            [styles.stepDisable]: step.status === StepStatus.DISABLED,
+            [styles.stepDark]: step.status === StepStatus.DARK || step.status === StepStatus.COMPLETED,
+          })}
+          key={step.value}
+        >
+          {step.status === StepStatus.COMPLETED ? <CheckIcon /> : <span>{step.value}</span>}
+        </div>
+      ))}
+    </div>
+  );
+};
