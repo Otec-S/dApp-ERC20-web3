2d8e72d09097d48077770ea37d6a52c8cdd29635
Galia Muslimova <galia.musllimova@gmail.com>
2024-09-30 18:34:33 +0500

    add hook for chain constants

--- src/components/IncomingOfferBlock/IncomingOfferBlock.tsx
+++ src/components/IncomingOfferBlock/IncomingOfferBlock.tsx
@@ -1,40 +1,37 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { useForm } from 'react-hook-form';
 import { useSearchParams } from 'react-router-dom';
 import { BarLoader } from 'react-spinners';
 import { Address, erc20Abi, formatUnits } from 'viem';
-import { polygonAmoy, sepolia } from 'viem/chains';
-import { useChainId, useReadContract, useReadContracts, useWriteContract } from 'wagmi';
+import { useAccount, useReadContract, useReadContracts, useWriteContract } from 'wagmi';
 
 import { ArrowRightIcon, NewWindowIcon, NotFoundIcon, SuccessIcon } from '@src/assets/icons';
 import { tradeContractAbi } from '@src/shared/constants';
+import { useChainDependentValues } from '@src/shared/hooks';
 
 import FormButton from '../form-button/FormButton';
 import { StepPagination } from '../StepPagination/StepPagination';
 import { StepStatus } from '../StepPagination/StepPagination.interface';
-import { getTokens } from '../TokensBlock/TokensBlock.utils';
 import styles from './IncomingOfferBlock.module.css';
 
-// const link = 'http://localhost:5173/offer?tokenFrom=0x0de27cBf804F1665eBc2F927944f54aA70cB4fC1&tokenTo=0xaa79133956a0F53Ef774c5b0e302784caF4A8Cc2&amountFrom=1&amountTo=3.5&tradeId=1&optionalTaker=0x0000000000000000000000000000000000000000';
-// const link = 'http://localhost:5173/offer?tradeId=1';
-
-const polygonAddress = '0x30bbAA44F7A80AfC9a5CB1aEEb8247B8E2aDE392';
+// const link = 'http://localhost:5173/offer?tradeId=4';
 
 export const IncomingOfferBlock: FC = () => {
   const [searchParams] = useSearchParams();
-  const chainId = useChainId();
+  const { address: walletAddress } = useAccount();
   const { handleSubmit } = useForm();
+  const { contractAddress, tokens, website } = useChainDependentValues();
 
-  const isExistChain = chainId === sepolia.id || chainId === polygonAmoy.id;
-  const contractAddress: Address = polygonAddress;
-  const tokens = isExistChain ? getTokens(chainId) : null;
   const tradeId = searchParams.get('tradeId');
+
+  const [isErrorShown, setIsErrorShown] = useState(false);
 
   const {
     writeContract: approveTrade,
     isPending: isApprovePending,
-    isError: isApproveError,
     isSuccess: isApproveSuccess,
+    isError: isApproveError,
   } = useWriteContract();
 
   const {
@@ -42,13 +39,12 @@ export const IncomingOfferBlock: FC = () => {
     data: acceptedTradeHash,
     isPending: isAcceptPending,
     isError: isAcceptError,
-    isSuccess: isAcceptSuccess,
   } = useWriteContract();
 
   const {
     data: offerData,
     isPending: isOfferDataPending,
-    error: offerDataError,
+    isError: isOfferDataError,
   } = useReadContract({
     abi: tradeContractAbi,
     address: contractAddress,
@@ -56,26 +52,33 @@ export const IncomingOfferBlock: FC = () => {
     args: [tradeId],
   });
 
-  const [tokenFrom, tokenTo, amountFrom, amountTo, creator, fee, optionalTaker] = offerData || [];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [tokenFrom, tokenTo, amountFrom, amountTo, creator, fee, optionalTaker, active, completed] = offerData || [];
 
   const { data: tokenFromData } = useReadContracts({
     allowFailure: false,
     contracts: tokenFrom &&
-      tokenTo && [
+      tokenTo &&
+      walletAddress && [
         { abi: erc20Abi, address: tokenFrom, functionName: 'decimals' },
         { abi: erc20Abi, address: tokenFrom, functionName: 'name' },
         { abi: erc20Abi, address: tokenTo, functionName: 'decimals' },
         { abi: erc20Abi, address: tokenTo, functionName: 'name' },
+        { abi: erc20Abi, address: tokenTo, functionName: 'allowance', args: [walletAddress, contractAddress] },
       ],
   });
 
+  const [tokenFromDecimals, tokenFromName, tokenToDecimals, tokenToName, allowance] = tokenFromData || [];
+  const isApproved = allowance || isApproveSuccess;
+
+  useEffect(() => {
+    setIsErrorShown(isOfferDataError || isApproveError || isAcceptError);
+  }, [isAcceptError, isApproveError, isOfferDataError]);
+
   if (isOfferDataPending || isAcceptPending || isApprovePending) return <BarLoader />;
 
-  if (offerDataError || isApproveError || isAcceptError) return <div>Error</div>;
-
-  const [tokenFromDecimals, tokenFromName, tokenToDecimals, tokenToName] = tokenFromData || [];
-  const amountFromFormatted = amountFrom && tokenFromDecimals && Number(formatUnits(amountFrom, tokenFromDecimals));
-  const amountToFormatted = amountTo && tokenToDecimals && Number(formatUnits(amountTo, tokenToDecimals));
+  const amountFromFormatted = Number(amountFrom && tokenFromDecimals && formatUnits(amountFrom, tokenFromDecimals));
+  const amountToFormatted = Number(amountTo && tokenToDecimals && formatUnits(amountTo, tokenToDecimals));
   const rate = amountFromFormatted && amountToFormatted && (amountToFormatted / amountFromFormatted).toFixed(2);
   const steps = [
     { value: 1, status: isApproveSuccess ? StepStatus.COMPLETED : StepStatus.DARK },
@@ -83,26 +86,30 @@ export const IncomingOfferBlock: FC = () => {
   ];
 
   const handleApproveTrade = () => {
-    if (amountFrom && tokenFrom) {
+    if (amountTo && tokenTo) {
       approveTrade({
         abi: erc20Abi,
-        address: tokenFrom,
+        address: tokenTo,
         functionName: 'approve',
-        args: [contractAddress, amountFrom],
+        args: [contractAddress, amountTo],
       });
     }
   };
+
+  console.log(contractAddress, tradeId);
 
   const handleAcceptTrade = () => {
     acceptTrade({
       abi: tradeContractAbi,
       address: contractAddress,
       functionName: 'take',
-      args: [0, tradeId],
+      args: [tradeId],
     });
   };
 
-  const handleViewTransaction = () => {};
+  const handleViewTransaction = () => {
+    window.open(`https://${website}/tx/${acceptedTradeHash}`, '_blank');
+  };
 
   const renderToken = ({
     address,
@@ -131,104 +138,126 @@ export const IncomingOfferBlock: FC = () => {
   };
 
   const renderSuccessDialog = () => {
-    if (amountFromFormatted && amountToFormatted && rate)
+    if (amountFromFormatted && amountToFormatted && rate) {
       return (
-        <div className={styles.container}>
-          <div className={styles.titleBlock}>
-            <h3 className={styles.title}>Offer has been successfully accepted!</h3>
-          </div>
-          <div className={styles.successContainer}>
-            <SuccessIcon />
-            <div className={styles.successInfoRow}>
-              <p className={styles.infoText}>{amountFromFormatted}</p>
-              <p className={styles.infoText}>{tokenFromName}</p>
-              <div className={styles.arrowBox}>
-                <ArrowRightIcon />
-              </div>
-              <p className={styles.infoText}>{amountToFormatted}</p>
-              <p className={styles.infoText}>{tokenToName}</p>
-              <p className={styles.infoText}>Rate</p>
-              <p className={styles.infoText}>{rate}</p>
+        <div className={styles.successContainer}>
+          <SuccessIcon />
+          <div className={styles.successInfoRow}>
+            <p className={styles.infoText}>{amountFromFormatted}</p>
+            <p className={styles.infoText}>{tokenFromName}</p>
+            <div className={styles.arrowBox}>
+              <ArrowRightIcon />
             </div>
+            <p className={styles.infoText}>{amountToFormatted}</p>
+            <p className={styles.infoText}>{tokenToName}</p>
+            <p className={styles.infoText}>Rate</p>
+            <p className={styles.infoText}>{rate}</p>
+          </div>
+          {acceptedTradeHash && (
             <button type="button" className={styles.newWindowButton} onClick={handleViewTransaction}>
               View transaction <NewWindowIcon />
             </button>
-            <FormButton colorScheme="yellow" type="button" buttonText="Great!" className={styles.greatButton} />
-          </div>
+          )}
+          <FormButton colorScheme="yellow" type="button" buttonText="Great!" className={styles.greatButton} />
         </div>
       );
+    }
+    return null;
   };
 
   const renderForm = () => {
     return (
-      <form onSubmit={handleSubmit(handleAcceptTrade)}>
-        <div className={styles.container}>
-          <div className={styles.titleBlock}>
-            <h3 className={styles.title}>Offer ID {tradeId}</h3>
-          </div>
-          <div className={styles.infoBlock}>
-            <div className={styles.tokenRow}>
-              {renderToken({
-                address: tokenFrom,
-                amount: amountFromFormatted,
-                text: 'You pay ',
-                symbol: tokenFromName,
-              })}
-              <ArrowRightIcon />
-              {renderToken({ address: tokenTo, amount: amountToFormatted, text: 'You get ', symbol: tokenToName })}
-              <div className={styles.rateBox}>
-                <div className={styles.infoCol}>
-                  <p className={styles.infoText}>{rate}</p>
-                  <p className={styles.infoDescription}>Rate</p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.infoBox}>
-              <div className={styles.arrowBox}>
-                <ArrowRightIcon />
-              </div>
+      <>
+        <div className={styles.infoBlock}>
+          <div className={styles.tokenRow}>
+            {renderToken({
+              address: tokenTo,
+              amount: amountToFormatted,
+              text: 'You pay ',
+              symbol: tokenToName,
+            })}
+            <ArrowRightIcon />
+            {renderToken({
+              address: tokenFrom,
+              amount: amountFromFormatted,
+              text: 'You get ',
+              symbol: tokenFromName,
+            })}
+            <div className={styles.rateBox}>
               <div className={styles.infoCol}>
-                <p className={styles.infoText}>{optionalTaker}</p>
-                <p className={styles.infoDescription}>Receiver</p>
+                <p className={styles.infoText}>{rate}</p>
+                <p className={styles.infoDescription}>Rate</p>
               </div>
             </div>
           </div>
-          <div className={styles.approveBlock}>
-            <input type="checkbox" name="infiniteApprove" />
-            <label htmlFor="infiniteApprove" className={styles.approveText}>
-              Infinite approve
-            </label>
-          </div>
-          <div className={styles.buttonBlock}>
-            <p className={styles.description}>You will have to sign 2 transactions: Approval of token & Accept Trade</p>
-            {isApproveSuccess ? (
-              <div className={styles.buttonRow}>
-                <FormButton
-                  colorScheme="yellow"
-                  buttonText="Accept Trade"
-                  className={styles.button}
-                  onClick={handleAcceptTrade}
-                />
-              </div>
-            ) : (
-              <div className={styles.buttonRow}>
-                <FormButton
-                  colorScheme="yellow"
-                  buttonText="Approve Token"
-                  className={styles.button}
-                  onClick={handleApproveTrade}
-                />
-                <FormButton type="button" buttonText="Accept Trade" disabled className={styles.button} />
-              </div>
-            )}
-            <div className={styles.paginationRow}>
-              <StepPagination steps={steps} />
+          <div className={styles.infoBox}>
+            <div className={styles.arrowBox}>
+              <ArrowRightIcon />
+            </div>
+            <div className={styles.infoCol}>
+              <p className={styles.infoText}>{optionalTaker}</p>
+              <p className={styles.infoDescription}>Receiver</p>
             </div>
           </div>
         </div>
-      </form>
+        <div className={styles.approveBlock}>
+          <input type="checkbox" name="infiniteApprove" />
+          <label htmlFor="infiniteApprove" className={styles.approveText}>
+            Infinite approve
+          </label>
+        </div>
+        <div className={styles.buttonBlock}>
+          <p className={styles.description}>You will have to sign 2 transactions: Approval of token & Accept Trade</p>
+          {isApproved ? (
+            <div className={styles.buttonRow}>
+              <FormButton
+                colorScheme="yellow"
+                buttonText="Accept Trade"
+                className={styles.button}
+                onClick={handleAcceptTrade}
+              />
+            </div>
+          ) : (
+            <div className={styles.buttonRow}>
+              <FormButton
+                colorScheme="yellow"
+                buttonText="Approve Token"
+                className={styles.button}
+                onClick={handleApproveTrade}
+              />
+              <FormButton type="button" buttonText="Accept Trade" disabled className={styles.button} />
+            </div>
+          )}
+          <div className={styles.paginationRow}>
+            <StepPagination steps={steps} />
+          </div>
+        </div>
+      </>
     );
   };
-  //   return renderSuccessDialog();
-  return isAcceptSuccess ? renderSuccessDialog() : renderForm();
+
+  return (
+    <form onSubmit={handleSubmit(handleAcceptTrade)}>
+      <div className={styles.container}>
+        <div className={styles.titleBlock}>
+          <h3 className={styles.title}>
+            {completed ? 'Offer has been successfully accepted!' : `Offer ID ${tradeId}`}
+          </h3>
+        </div>
+        {completed ? renderSuccessDialog() : renderForm()}
+        {isErrorShown &&
+          ReactDOM.createPortal(
+            <div className={styles.errorWrap}>
+              <div className={styles.errorContainer}>
+                <div className={styles.successLogoWrapper}>
+                  <h5 className={styles.header}>{'Something went wrong'}</h5>
+                </div>
+                <FormButton colorScheme={'yellow'} onPointerDown={() => setIsErrorShown(false)} buttonText="Back" />
+              </div>
+            </div>,
+            document.getElementsByClassName('app')[0],
+          )}
+      </div>
+    </form>
+  );
 };
