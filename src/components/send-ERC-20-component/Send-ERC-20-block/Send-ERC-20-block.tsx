2f92479382da7f9345aa805cb3266cbc6bb80690
Sergey Grigorash <adviser@bk.ru>
2024-10-01 09:37:39 +0500

    fix move the rest of states down from page level SendERC20 to block level

--- src/components/send-ERC-20-component/Send-ERC-20-block/Send-ERC-20-block.tsx
+++ src/components/send-ERC-20-component/Send-ERC-20-block/Send-ERC-20-block.tsx
@@ -1,73 +1,71 @@
 import { FC, useState } from 'react';
 import { Address } from 'viem';
 
+import AddTokenInfo from '@src/components/add-token-info-popup/AddTokenInfo';
 import { TokenData } from '@src/shared/constants';
 
 import SendERC20ResultForm from '../Send-ERC-20-result-form/Send-ERC-20-result-form';
 import SendERC20SendForm from '../Send-ERC-20-send-form/Send-ERC-20-send-form';
 import style from './Send-ERC-20-block.module.css';
 
-interface Props {
-  setIsCustomTokenPopupOpen: (value: boolean) => void;
-  tokenData: TokenData | null;
-  setTokenData: (value: TokenData | null) => void;
-  tokenName: string;
-  setTokenName: (value: string) => void;
-}
-
-const SendERC20Block: FC<Props> = ({ setIsCustomTokenPopupOpen, tokenData, setTokenData, tokenName, setTokenName }) => {
+const SendERC20Block: FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
   const [txHash, setTxHash] = useState<Address>('0x0');
-  console.log('txHash:', txHash);
-
   const [isTxSuccess, setIsTxSuccess] = useState(true);
   const [isTxFormSubmitted, setIsTxFormSubmitted] = useState(false);
+  const [isCustomTokenPopupOpen, setIsCustomTokenPopupOpen] = useState(false);
+  const [tokenData, setTokenData] = useState<TokenData | null>(null); // Состояние для хранения данных токена
+  const [tokenName, setTokenName] = useState<string>('ARB');
 
-  // TODO:
-
-  {
-    /* <div className=" стили с position:relative например чтобы popup с position:absolute показывался внутри него. У нас у всех popup вроде position:absolute установлено сразу в стилях">
-{
-isCustomTokenPopupOpen && <AddTokenInfo  onClosePopup = {твой хендлер чтобы получить из popup сведения} >
-}
-</div> */
-  }
+  const handleCloseCustomTokenPopup = (data: TokenData) => {
+    setIsCustomTokenPopupOpen(false);
+    if (data && data.tokenName) {
+      setTokenData(data);
+      setTokenName(data.tokenName);
+    }
+  };
 
   return (
-    <section className={style.block}>
-      <div className={style.blockTitle}>
-        {isTxFormSubmitted
-          ? isTxSuccess
-            ? 'Tokens have been successfully sent!'
-            : 'Something went wrong'
-          : 'Send ERC-20'}
-      </div>
-
-      {isTxFormSubmitted ? (
-        <SendERC20ResultForm
-          isTxSuccess={isTxSuccess}
-          setIsTxFormSubmitted={setIsTxFormSubmitted}
-          inputValue={inputValue}
-          setInputValue={setInputValue}
-          tokenName={tokenName}
-          setTokenName={setTokenName}
-          setTokenData={setTokenData}
-          txHash={txHash}
-        />
-      ) : (
-        <SendERC20SendForm
-          setIsTxSuccess={setIsTxSuccess}
-          isTxFormSubmitted={isTxFormSubmitted}
-          setIsTxFormSubmitted={setIsTxFormSubmitted}
-          inputValue={inputValue}
-          setInputValue={setInputValue}
-          setTokenName={setTokenName}
-          setIsCustomTokenPopupOpen={setIsCustomTokenPopupOpen}
-          tokenData={tokenData}
-          setTxHash={setTxHash}
-        />
+    <>
+      <section className={style.block}>
+        <div className={style.blockTitle}>
+          {isTxFormSubmitted
+            ? isTxSuccess
+              ? 'Tokens have been successfully sent!'
+              : 'Something went wrong'
+            : 'Send ERC-20'}
+        </div>
+        {isTxFormSubmitted ? (
+          <SendERC20ResultForm
+            isTxSuccess={isTxSuccess}
+            setIsTxFormSubmitted={setIsTxFormSubmitted}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            tokenName={tokenName}
+            setTokenName={setTokenName}
+            setTokenData={setTokenData}
+            txHash={txHash}
+          />
+        ) : (
+          <SendERC20SendForm
+            setIsTxSuccess={setIsTxSuccess}
+            isTxFormSubmitted={isTxFormSubmitted}
+            setIsTxFormSubmitted={setIsTxFormSubmitted}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            setTokenName={setTokenName}
+            setIsCustomTokenPopupOpen={setIsCustomTokenPopupOpen}
+            tokenData={tokenData}
+            setTxHash={setTxHash}
+          />
+        )}
+      </section>
+      {isCustomTokenPopupOpen && (
+        <div className={style.overlay}>
+          <AddTokenInfo setIsCustomTokenPopupOpen={setIsCustomTokenPopupOpen} onClose={handleCloseCustomTokenPopup} />
+        </div>
       )}
-    </section>
+    </>
   );
 };
 
--- src/pages/SendERC20/send-ERC-20.tsx
+++ src/pages/SendERC20/send-ERC-20.tsx
@@ -1,44 +1,16 @@
-import { useState } from 'react';
-
-import AddTokenInfo from '@src/components/add-token-info-popup/AddTokenInfo';
-import { TokenData } from '@src/shared/constants';
-
 import Header from '../../components/header/Header';
 import SendERC20Block from '../../components/send-ERC-20-component/Send-ERC-20-block/Send-ERC-20-block';
 import style from './Send-ERC-20.module.css';
 
 const SendERC20 = () => {
-  const [isCustomTokenPopupOpen, setIsCustomTokenPopupOpen] = useState(false);
-  const [tokenData, setTokenData] = useState<TokenData | null>(null); // Состояние для хранения данных токена
-  const [tokenName, setTokenName] = useState<string>('ARB');
-
-  const handleCloseCustomTokenPopup = (data: TokenData) => {
-    setIsCustomTokenPopupOpen(false);
-    if (data && data.tokenName) {
-      setTokenData(data);
-      setTokenName(data.tokenName);
-    }
-  };
-
   return (
     <>
       <div className={style.header}>
         <Header />
       </div>
       <div className={style.component}>
-        <SendERC20Block
-          setIsCustomTokenPopupOpen={setIsCustomTokenPopupOpen}
-          tokenData={tokenData} //6
-          setTokenData={setTokenData} //7
-          tokenName={tokenName} //8
-          setTokenName={setTokenName} //9
-        />
+        <SendERC20Block />
       </div>
-      {isCustomTokenPopupOpen && (
-        <div className={style.overlay}>
-          <AddTokenInfo setIsCustomTokenPopupOpen={setIsCustomTokenPopupOpen} onClose={handleCloseCustomTokenPopup} />
-        </div>
-      )}
     </>
   );
 };
