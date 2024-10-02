b19ebe730bf579b33514d330555ab3941ca755f5
Vadim Manushin <vadimmanushin@yandex.ru>
2024-10-02 13:31:22 +0300

    deleted element from main page

--- src/pages/ERC20send/ERC20send.tsx
+++ src/pages/ERC20send/ERC20send.tsx
@@ -1,21 +1,11 @@
 import { FC } from 'react';
 
-import CancelOffer from '@src/components/cancel-offer-popup/CancelOffer';
-
 import Header from '../../components/header/Header';
 
 export const ERC20send: FC = () => {
   return (
     <div style={{ background: '#FFEDBE' }}>
       <Header colorScheme="lightBackground" />
-      <CancelOffer
-        tokenFromName="MKR"
-        tokenToName="WETH"
-        amountFrom={16.56}
-        amountTo={18.0}
-        onClose={(data) => console.log(data)}
-        tradeId={BigInt(13)}
-      />
     </div>
   );
 };
