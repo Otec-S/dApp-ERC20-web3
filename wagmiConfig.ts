1bb0c73b3c5ecde2973d277b964a4452db835186
Sergey Grigorash <adviser@bk.ru>
2024-10-03 13:20:26 +0500

    ref make transports from Alchemy

--- wagmiConfig.ts
+++ wagmiConfig.ts
@@ -1,16 +1,15 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-// import { http } from 'wagmi';
+import { http } from '@wagmi/core';
 import { mainnet, polygon, polygonAmoy, sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'Scrum Team 17 web3 kEthers fans',
   projectId: import.meta.env.VITE_WAGMI_PROJECT_ID,
   chains: [mainnet, polygon, sepolia, polygonAmoy],
-  // TODO:
-  // transports: {
-  //   [mainnet.id]: http(),
-  //   [sepolia.id]: http(),
-  //   [polygon.id]: http(),
-  //   [polygonAmoy.id]: http(),
-  // },
+  transports: {
+    [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/5opf7-1xwOLzLDlRFmXAeQgbPdymDv02'),
+    [polygon.id]: http('https://polygon-mainnet.g.alchemy.com/v2/5opf7-1xwOLzLDlRFmXAeQgbPdymDv02'),
+    [polygonAmoy.id]: http('https://polygon-amoy.g.alchemy.com/v2/5opf7-1xwOLzLDlRFmXAeQgbPdymDv02'),
+    [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/5opf7-1xwOLzLDlRFmXAeQgbPdymDv02'),
+  },
 });
