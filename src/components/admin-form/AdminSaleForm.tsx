90842820f9650f9af889664bd64d10642a9405e9
Vadim Manushin <vadimmanushin@yandex.ru>
2024-10-13 13:04:21 +0300

    prettier

--- src/components/admin-form/AdminSaleForm.tsx
+++ src/components/admin-form/AdminSaleForm.tsx
@@ -9,45 +9,63 @@ export const AdminSaleForm: FC = () => {
         <legend className={styles.label}>Select airdrop state:</legend>
         <div>
           <input type="radio" id="airdrope0" name="airdrope" value="0" />
-          <label className={styles.label} htmlFor="airdrope0">Soon</label>
+          <label className={styles.label} htmlFor="airdrope0">
+            Soon
+          </label>
         </div>
         <div>
           <input type="radio" id="airdrope1" name="airdrope" value="1" />
-          <label className={styles.label} htmlFor="airdrope1">Available</label>
+          <label className={styles.label} htmlFor="airdrope1">
+            Available
+          </label>
         </div>
         <div>
           <input type="radio" id="airdrope2" name="airdrope" value="2" />
-          <label  className={styles.label} htmlFor="airdrope2">Finished</label>
+          <label className={styles.label} htmlFor="airdrope2">
+            Finished
+          </label>
         </div>
       </fieldset>
       <fieldset className={styles.fieldSet}>
-        <legend className={styles.label} >Select white list state:</legend>
+        <legend className={styles.label}>Select white list state:</legend>
         <div>
           <input type="radio" id="whitelist0" name="whitelist" value="0" />
-          <label className={styles.label} htmlFor="whitelist0">Soon</label>
+          <label className={styles.label} htmlFor="whitelist0">
+            Soon
+          </label>
         </div>
         <div>
           <input type="radio" id="whitelist1" name="whitelist" value="1" />
-          <label className={styles.label} htmlFor="whitelist1">Available</label>
+          <label className={styles.label} htmlFor="whitelist1">
+            Available
+          </label>
         </div>
         <div>
           <input type="radio" id="whitelist2" name="whitelist" value="2" />
-          <label className={styles.label} htmlFor="whitelist2">Finished</label>
+          <label className={styles.label} htmlFor="whitelist2">
+            Finished
+          </label>
         </div>
       </fieldset>
       <fieldset className={styles.fieldSet}>
         <legend className={styles.label}>Select public sale state:</legend>
         <div>
           <input type="radio" id="publicSale0" name="publicSale" value="0" />
-          <label className={styles.label} htmlFor="publicSale0">Soon</label>
+          <label className={styles.label} htmlFor="publicSale0">
+            Soon
+          </label>
         </div>
         <div>
           <input type="radio" id="publicSale1" name="publicSale" value="1" />
-          <label className={styles.label} htmlFor="publicSale1">Available</label>
+          <label className={styles.label} htmlFor="publicSale1">
+            Available
+          </label>
         </div>
         <div>
           <input type="radio" id="publicSale2" name="publicSale" value="2" />
-          <label className={styles.label} htmlFor="publicSale2">Finished</label>
+          <label className={styles.label} htmlFor="publicSale2">
+            Finished
+          </label>
         </div>
       </fieldset>
     </>
