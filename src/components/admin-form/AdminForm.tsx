2eb9c91554a4476a7da80bc83831b026a15e18a2
Vadim Manushin <vadimmanushin@yandex.ru>
2024-10-13 19:24:59 +0300

    logic of showing admin form removed from page into form

--- src/components/admin-form/AdminForm.tsx
+++ src/components/admin-form/AdminForm.tsx
@@ -1,14 +1,135 @@
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BeatLoader } from 'react-spinners';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { arbitrumSepolia } from 'viem/chains';
+import { useAccount, useReadContracts } from 'wagmi';
+
+import FormButton from '@components/form-button/FormButton';
+import { ROUTES } from '@shared/constants';
+import { nftContractAddress } from '@shared/constants/nftContract';
+import { nftContractAbi } from '@shared/constants/nftContractAbi';
 
 import { AdminSaleForm } from './AdminSaleForm';
 import styles from './AdminForm.module.css';
 
-interface Props {
-  priceManager: boolean;
-  saleManager: boolean;
-  whiteListManager: boolean;
-}
+const override: CSSProperties = {
+  display: 'block',
+  margin: '100px auto',
+};
 
-export const AdminForm: FC<Props> = ({ priceManager, saleManager, whiteListManager }) => {
-  return <div className={styles.adminForm}>{saleManager && <AdminSaleForm />}</div>;
+export const AdminForm: FC = () => {
+  const { isConnected, chainId, address: walletAddress } = useAccount();
+  const { openConnectModal } = useConnectModal();
+  const navigate = useNavigate();
+
+  if (!isConnected && openConnectModal) {
+    openConnectModal();
+  }
+
+  const { data: constantsRoles, isLoading: isConstantsLoading } = useReadContracts({
+    allowFailure: false,
+    contracts: [
+      {
+        address: nftContractAddress,
+        functionName: 'PRICE_MANAGER_ROLE',
+        abi: nftContractAbi,
+      },
+      {
+        address: nftContractAddress,
+        functionName: 'SELL_PHASE_MANAGER_ROLE',
+        abi: nftContractAbi,
+      },
+      {
+        address: nftContractAddress,
+        functionName: 'WHITE_LIST_MANAGER_ROLE',
+        abi: nftContractAbi,
+      },
+    ],
+  });
+
+  const PRICE_MANAGER_ROLE_DESCRIPTOR = constantsRoles && constantsRoles[0];
+  const SELL_PHASE_MANAGER_ROLE_DESCRIPTOR = constantsRoles && constantsRoles[1];
+  const WHITE_LIST_MANAGER_ROLE_DESCRIPTOR = constantsRoles && constantsRoles[2];
+
+  const { data: roles, isLoading: isRolesApprovedLoading } = useReadContracts({
+    allowFailure: false,
+    contracts: [
+      {
+        address: nftContractAddress,
+        functionName: 'hasRole',
+        abi: nftContractAbi,
+        args: PRICE_MANAGER_ROLE_DESCRIPTOR && walletAddress && [PRICE_MANAGER_ROLE_DESCRIPTOR, walletAddress],
+      },
+      {
+        address: nftContractAddress,
+        functionName: 'hasRole',
+        abi: nftContractAbi,
+        args: SELL_PHASE_MANAGER_ROLE_DESCRIPTOR &&
+          walletAddress && [SELL_PHASE_MANAGER_ROLE_DESCRIPTOR, walletAddress],
+      },
+      {
+        address: nftContractAddress,
+        functionName: 'hasRole',
+        abi: nftContractAbi,
+        args: WHITE_LIST_MANAGER_ROLE_DESCRIPTOR &&
+          walletAddress && [WHITE_LIST_MANAGER_ROLE_DESCRIPTOR, walletAddress],
+      },
+    ],
+  });
+
+  // const isUserPriceManager=roles && roles[0]; 
+  const isUserSaleManager= roles && roles[1]; 
+  // const isUserWhiteListManager= roles && roles[2];
+
+  const dataIsLoading = isRolesApprovedLoading || isConstantsLoading;
+
+  if (dataIsLoading) {
+    return (
+      <div className={styles.loader}>
+        <BeatLoader
+          color={'red'}
+          loading={true}
+          cssOverride={override}
+          size={100}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+    );
+  }
+
+  const isUserAllowedToPassAdminPanel = roles && roles.some((role) => role === true);
+
+  if (!isUserAllowedToPassAdminPanel) {
+    return (
+      <div className={styles.admin}>
+        <main className={styles.main}>
+          <h1 className={styles.header}>You are not allowed to admin panel</h1>
+          <h2 className={styles.subheader}>Please check your rights and chain settings(Arbitrum Sepolia)</h2>
+          <div className={styles.backButton}>
+            <FormButton colorScheme="yellow" buttonText="Back" onPointerDown={() => navigate(ROUTES.HOME)} />
+          </div>
+        </main>
+      </div>
+    );
+  }
+
+  if (chainId === arbitrumSepolia.id) {
+    return (
+
+        <section className={styles.main}>
+          <h2 className={styles.header}>Admin form</h2>
+          <div className={styles.adminForm}>{isUserSaleManager && <AdminSaleForm />}</div>
+        </section>
+    );
+  }
+
+  return (
+    <div className={styles.admin}>
+      <main className={styles.main}>
+        <h1 className={styles.header}>Please select arbitrum sepolia network</h1>
+      </main>
+    </div>
+  );
 };
--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,136 +1,17 @@
-import { CSSProperties, FC } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { BeatLoader } from 'react-spinners';
-import { useConnectModal } from '@rainbow-me/rainbowkit';
-import { arbitrumSepolia } from 'viem/chains';
-import { useAccount, useReadContracts } from 'wagmi';
+import { FC } from 'react';
 
 import { AdminForm } from '@components/admin-form/AdminForm';
-import FormButton from '@components/form-button/FormButton';
 import Header from '@components/header/Header';
-import { ROUTES } from '@shared/constants';
-import { nftContractAddress } from '@shared/constants/nftContract';
-import { nftContractAbi } from '@shared/constants/nftContractAbi';
 
 import styles from './Admin.module.css';
 
-const override: CSSProperties = {
-  display: 'block',
-  margin: '100px auto',
-};
-
 export const Admin: FC = () => {
-  const { isConnected, chainId, address: walletAddress } = useAccount();
-  const { openConnectModal } = useConnectModal();
-  const navigate = useNavigate();
-
-  if (!isConnected && openConnectModal) {
-    openConnectModal();
-  }
-
-  const { data: constantsRoles, isLoading: isConstantsLoading } = useReadContracts({
-    allowFailure: false,
-    contracts: [
-      {
-        address: nftContractAddress,
-        functionName: 'PRICE_MANAGER_ROLE',
-        abi: nftContractAbi,
-      },
-      {
-        address: nftContractAddress,
-        functionName: 'SELL_PHASE_MANAGER_ROLE',
-        abi: nftContractAbi,
-      },
-      {
-        address: nftContractAddress,
-        functionName: 'WHITE_LIST_MANAGER_ROLE',
-        abi: nftContractAbi,
-      },
-    ],
-  });
-
-  const PRICE_MANAGER_ROLE_DESCRIPTOR = constantsRoles && constantsRoles[0];
-  const SELL_PHASE_MANAGER_ROLE_DESCRIPTOR = constantsRoles && constantsRoles[1];
-  const WHITE_LIST_MANAGER_ROLE_DESCRIPTOR = constantsRoles && constantsRoles[2];
-
-  const { data: roles, isLoading: isRolesApprovedLoading } = useReadContracts({
-    allowFailure: false,
-    contracts: [
-      {
-        address: nftContractAddress,
-        functionName: 'hasRole',
-        abi: nftContractAbi,
-        args: PRICE_MANAGER_ROLE_DESCRIPTOR && walletAddress && [PRICE_MANAGER_ROLE_DESCRIPTOR, walletAddress],
-      },
-      {
-        address: nftContractAddress,
-        functionName: 'hasRole',
-        abi: nftContractAbi,
-        args: SELL_PHASE_MANAGER_ROLE_DESCRIPTOR &&
-          walletAddress && [SELL_PHASE_MANAGER_ROLE_DESCRIPTOR, walletAddress],
-      },
-      {
-        address: nftContractAddress,
-        functionName: 'hasRole',
-        abi: nftContractAbi,
-        args: WHITE_LIST_MANAGER_ROLE_DESCRIPTOR &&
-          walletAddress && [WHITE_LIST_MANAGER_ROLE_DESCRIPTOR, walletAddress],
-      },
-    ],
-  });
-
-  const dataIsLoading = isRolesApprovedLoading || isConstantsLoading;
-
-  if (dataIsLoading) {
-    return (
-      <div className={styles.loader}>
-        <BeatLoader
-          color={'red'}
-          loading={true}
-          cssOverride={override}
-          size={100}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-      </div>
-    );
-  }
-
-  const isUserAllowedToPassAdminPanel = roles && roles.some((role) => role === true);
-
-  if (!isUserAllowedToPassAdminPanel) {
-    return (
-      <div className={styles.admin}>
-        <Header colorScheme="darkBackground" />
-        <main className={styles.main}>
-          <h1 className={styles.header}>You are not allowed to admin panel</h1>
-          <h2 className={styles.subheader}>Please check your rights and chain settings(Arbitrum Sepolia)</h2>
-          <div className={styles.backButton}>
-            <FormButton colorScheme="yellow" buttonText="Back" onPointerDown={() => navigate(ROUTES.HOME)} />
-          </div>
-        </main>
-      </div>
-    );
-  }
-
-  if (chainId === arbitrumSepolia.id) {
-    return (
-      <div className={styles.admin}>
-        <Header colorScheme="darkBackground" />
-        <main className={styles.main}>
-          <h1 className={styles.header}>Admin page</h1>
-          <AdminForm priceManager={roles[0]} saleManager={roles[1]} whiteListManager={roles[2]} />
-        </main>
-      </div>
-    );
-  }
-
+  
   return (
     <div className={styles.admin}>
       <Header colorScheme="darkBackground" />
-      <main className={styles.main}>
-        <h1 className={styles.header}>Please select arbitrum sepolia network</h1>
-      </main>
+      <h1 className={styles.header}>Admin page</h1>
+      <AdminForm />
     </div>
   );
 };
