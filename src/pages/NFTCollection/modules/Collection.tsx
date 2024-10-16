f4963f65cdeb7ca79c715f84fd199321d424fb69
Galia Muslimova <galia.musllimova@gmail.com>
2024-10-16 09:06:08 +0500

    create nft collection page

--- src/pages/NFTCollection/modules/Collection.tsx
+++ src/pages/NFTCollection/modules/Collection.tsx
@@ -1,5 +1,30 @@
 import { FC } from 'react';
+import { useAccount, useReadContracts } from 'wagmi';
+
+import { nftContractAbi } from '@shared/constants';
+import { useChainDependentValues } from '@shared/hooks';
+
+import styles from '../NFTCollection.module.css';
 
 export const Collection: FC = () => {
-  return <div />;
+  const { address: walletAddress } = useAccount();
+  const { nftContractAddress } = useChainDependentValues();
+
+  const { data } = useReadContracts({
+    allowFailure: false,
+    contracts: walletAddress
+      ? [{ abi: nftContractAbi, address: nftContractAddress, functionName: 'tokensOfOwner', args: [walletAddress] }]
+      : undefined,
+  });
+
+  const [tokenIds] = data || [];
+
+  return (
+    <div>
+      <div className={styles.titleWrap}>
+        <h2 className={styles.title}>Your minted NFT</h2>
+        <div className={styles.collection}></div>
+      </div>
+    </div>
+  );
 };
