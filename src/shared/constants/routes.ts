a67deccb637d08f2ed268e262bb7d76378f32f66
Vadim Manushin <vadimmanushin@yandex.ru>
2024-10-16 20:30:26 +0300

    fix route path

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -2,7 +2,7 @@ export const ROUTES = {
   HOME: '/',
   SEND_ERC20: '/send-ERC20-tokens',
   ERC20_TRADE: '/erc20trade',
-  NFT_COLLECTION: '/',
+  NFT_COLLECTION: '/nft',
   ADMIN: '/admin',
 
   CREATE_OFFER: '/erc20trade/create-offer',
