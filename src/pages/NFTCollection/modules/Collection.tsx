cfa09cf215a143a3aac9357ae55df0bb7ae2e0ec
Galia Muslimova <galia.musllimova@gmail.com>
2024-10-16 10:11:04 +0500

    add images of collection

--- src/pages/NFTCollection/modules/Collection.tsx
+++ src/pages/NFTCollection/modules/Collection.tsx
@@ -1,16 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useAccount, useReadContracts } from 'wagmi';
 
 import { nftContractAbi } from '@shared/constants';
-import { useChainDependentValues } from '@shared/hooks';
+import { useChainDependentValues, useFetchFiles } from '@shared/hooks';
 
 import styles from '../NFTCollection.module.css';
+import { Loader } from '@components/loader/Loader';
+import { EthLogoIcon } from '@assets/icons';
 
 export const Collection: FC = () => {
   const { address: walletAddress } = useAccount();
   const { nftContractAddress } = useChainDependentValues();
+  const { files, setTokenIds } = useFetchFiles();
 
-  const { data } = useReadContracts({
+  const { data, isLoading } = useReadContracts({
     allowFailure: false,
     contracts: walletAddress
       ? [{ abi: nftContractAbi, address: nftContractAddress, functionName: 'tokensOfOwner', args: [walletAddress] }]
@@ -19,11 +22,34 @@ export const Collection: FC = () => {
 
   const [tokenIds] = data || [];
 
+  useEffect(() => {
+    setTokenIds(tokenIds ? tokenIds.map((item) => Number(item)) : []);
+  }, [tokenIds]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div>
       <div className={styles.titleWrap}>
         <h2 className={styles.title}>Your minted NFT</h2>
-        <div className={styles.collection}></div>
+      </div>
+      <div className={styles.collection}>
+        {files.map((file) => (
+          <div className={styles.contentImage}>
+            <div className={styles.imageWrap}>
+              <img className={styles.image} src={file.image}></img>
+            </div>
+            <div className={styles.imageInfo}>
+              <p className={styles.imageInfoText}>{file.name}</p>
+              <div className={styles.imageInfoCost}>
+                <p className={styles.imageInfoText}>ETH</p>
+                <EthLogoIcon />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/shared/hooks/useFetchFiles.ts
+++ src/shared/hooks/useFetchFiles.ts
@@ -4,38 +4,41 @@ import { download } from 'thirdweb/storage';
 
 export interface NFTFile {
   image: string;
+  name: string;
 }
 
 const uri = 'https://ipfs.io/ipfs/QmNe7EebKaNuRoN2ov9nMuwHXQvXbYXCLM9W1nDBxnqLgL/';
 
-export const useFetchFiles = ({ tokenIds }: { tokenIds: number[] }) => {
+export const useFetchFiles = () => {
   const clientId = import.meta.env.VITE_THIRD_WEB_CLIENT_ID ?? '';
   const client = createThirdwebClient({ clientId });
+  const [tokenIds, setTokenIds] = useState<number[]>([]);
 
   const [files, setFiles] = useState<NFTFile[]>([]);
 
-  const fetches: Promise<NFTFile>[] = tokenIds.map((id) => {
-    return new Promise((resolve) => {
-      download({
-        client,
-        uri: `${uri}${id}.json`,
-      }).then((file) => {
-        resolve(file.json());
+  useEffect(() => {
+    const fetches: Promise<NFTFile>[] = tokenIds.map((id) => {
+      return new Promise((resolve) => {
+        download({
+          client,
+          uri: `${uri}${id}.json`,
+        }).then((file) => {
+          resolve(file.json());
+        });
       });
     });
-  });
 
-  useEffect(() => {
     Promise.all(fetches).then((res) => {
       const formatData = res.map((item) => {
         const imageArr = item.image.split('/');
         return {
           image: `https://ipfs.io/ipfs/${imageArr[2]}/${imageArr[3]}`,
+          name: item.name,
         };
       });
       setFiles(formatData);
     });
-  }, []);
+  }, [tokenIds]);
 
-  return { files };
+  return { files, setTokenIds };
 };
